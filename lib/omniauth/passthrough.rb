5e70fc0c6235a6ad5d11b97d8c06f7978ea15e80
benmelz <ben@melz.me>
2025-02-03 14:55:21 -0500

    bundle gem

---
+++ lib/omniauth/passthrough.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "passthrough/version"
+
+module Omniauth
+  module Passthrough
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/omniauth/passthrough_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Omniauth::Passthrough do
+  it "has a version number" do
+    expect(Omniauth::Passthrough::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
